2c34ec3bc9ee7ac75d973e8120036cbe46a5e831
Avinash0957 <avinash.c@cecs.co.in>
2025-12-15 10:54:20 +0530

    changes added ejs for direct run in browser

---
+++ chat-gpt-clone/public/script.js
@@ -0,0 +1,76 @@
+const input = document.querySelector('#input');
+const askbtn = document.querySelector("#askbtn")
+const chatContainer = document.querySelector("#chat-container")
+input.addEventListener('keyup', handleEnter)
+askbtn.addEventListener('click', handleAsk)
+const max_retires = 5 ;
+let count = 0;
+const loadding = document.createElement("div");
+loadding.className = 'my-6 animate-pulse resize-none outline-0 p-3'
+loadding.textContent = 'Thinking............'
+
+async function handleEnter(e) {
+    if (e.key == "Enter") {
+        const text = input.value.trim();
+        if (!text) {
+            return
+        }
+        await Generate(text)
+    }
+}
+
+async function callserver(inputText) {
+    debugger;
+    count = count + 1 ;
+
+    while(count > max_retires){
+
+        return {Response : "Please try after sometime...."} ;
+    }
+   const threadId =  Date.now().toString(36) + Math.random().toString(36).substring(2,8);
+
+     try {
+    const response = await fetch('/Chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ message: inputText, threadId })
+    });
+    const data = await response.json();
+    console.log('Server Response:', data);
+
+    return data; 
+  } catch (error) {
+    console.error('Error calling server:', error);
+    return { Response: 'Error connecting to server.' };
+  }
+}
+async function handleAsk(e) {
+    const text = input.value.trim();
+    if (!text) {
+        return
+    }
+    await Generate(text)
+}
+
+async function Generate(text) {
+    const msg = document.createElement('div');
+    msg.className = `my-6 bg-neutral-800 p-3 rounded-xl ml-auto max-w-fit`;
+    msg.textContent = text;
+    chatContainer.appendChild(msg);
+    input.value = "";
+
+    chatContainer.appendChild(loadding);
+
+    const assistancemessege = await callserver(text);
+    console.log('assistancemessege', assistancemessege.Response);
+
+
+    const Assistantmsg = document.createElement('div');
+    Assistantmsg.className = `max-w-fit`;
+    Assistantmsg.textContent = assistancemessege.Response;
+
+    loadding.remove();
+    chatContainer.appendChild(Assistantmsg);
+}
--- chat-gpt-clone/server.js
+++ chat-gpt-clone/server.js
@@ -4,26 +4,15 @@ import cors from 'cors'
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
-const allowedOrigins = ["https://node-archivements.vercel.app"];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps or curl)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true // if you need cookies/auth
-}));
-//app.use(express.json())
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(express.json())
 app.get("/", (req, res) => {
 
-    res.send('Hello World !')
+    res.render('index', { 
+    title: 'Home Page',
+    message: 'Welcome to EJS in Node.js!' 
+  })
 })
 
 app.post("/Chat", async (req,res)=>{
